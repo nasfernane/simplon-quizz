026ef9142c11128aa678eca7b4c1f3e86c5a8bec
nasfernane <nasfernane@gmail.com>
2021-03-17 10:28:50 +0100

    push substr

--- php/views/login.php
+++ php/views/login.php
@@ -1,1 +1,25 @@
 <h1>login</h1>
+<div class="loginContainer">
+    <form action="" class="loginForm">
+        <div class="loginForm__field">
+            <label for="loginFormEmail">Votre adresse e-mail</label>
+            <input id="loginFormEmail" class="loginForm__field--email" type="email" placeholder="ex: maxime@leforstier.fr">
+        </div>
+
+        <div class="loginForm__field">
+            <label for="loginFormName">Votre nom</label>
+            <input type="text" id="loginFormName" class="loginForm__field--name">
+        </div>
+
+        <div class="loginForm__field">
+            <label for="loginFormPw">Votre mot de passe</label>
+            <input type="password" id="loginFormPw" class="loginForm__field--pw">
+        </div>
+
+        <div class="loginForm__field">
+            <label for="loginFormPwConfirm">Confirmation de votre mot de passe</label>
+            <input type="password" id="loginFormPwConfirm" class="loginForm__field--pwConfirm">
+        </div>
+    </form>
+    <p>Besoin d'un compte ? <a href="?page=signup">S'inscrire</a></p>
+</div>
--- routeur.php
+++ routeur.php
@@ -2,6 +2,8 @@
 
 $method = $_SERVER["REQUEST_METHOD"];                                                                 
 $path = $_SERVER["REQUEST_URI"];    
+// pour mathieu
+// $path = substr($_SERVER["REQUEST_URI"], 26);
 
 if ($method === "GET" && $path === "/") {                                                             
     require './php/views/home.php';     
